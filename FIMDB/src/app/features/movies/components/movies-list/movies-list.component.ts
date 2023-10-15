7943d0d8755a8b970af2b89035f0c20995afcc9e
anetastankovska <aneta.stankovska@cw.bluedotcorp.com>
2023-10-15 10:56:54 +0200

    add filter movies

--- FIMDB/src/app/features/movies/components/movies-list/movies-list.component.ts
+++ FIMDB/src/app/features/movies/components/movies-list/movies-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Movie } from '../../interfaces/movie.interface';
 import { MoviesService } from '../../services/movies.service';
 
@@ -7,21 +7,31 @@ import { MoviesService } from '../../services/movies.service';
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.scss']
 })
-export class MoviesListComponent implements OnInit{
-
+export class MoviesListComponent implements OnInit {
   public movies: Movie[] = [];
+  public filteredMovies: Movie[] = [];
   public isFetching: boolean = false;
 
-  constructor(private readonly moviesService: MoviesService) {
-  }
+  @ViewChild('searchInput') searchInput: ElementRef;
+
+  constructor(private readonly moviesService: MoviesService) {}
 
   ngOnInit(): void {
-    this.moviesService.getAllMovies().subscribe(movies => {
-      this.movies = movies
-      console.log(movies)
-    })
+    this.moviesService.getAllMovies().subscribe((movies) => {
+      this.movies = movies;
+      this.filteredMovies = movies;
+    });
 
     this.moviesService.isFetching.subscribe((isFetching) => (this.isFetching = isFetching));
   }
 
+  filterMovies(query: string) {
+    if (!query) {
+      this.filteredMovies = this.movies;
+    } else {
+      this.filteredMovies = this.movies.filter((movie) =>
+        movie.name.toLowerCase().includes(query.toLowerCase())
+      );
+    }
+  }
 }
